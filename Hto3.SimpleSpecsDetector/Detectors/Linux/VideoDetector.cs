b39fb2fe9c18002981bfdaaab08abb2fa0840e83
HTO3 <helio_net@hotmail.com>
2020-11-22 21:23:41 -0300

    Linux Video Detector methods.

--- Hto3.SimpleSpecsDetector/Detectors/Linux/VideoDetector.cs
+++ Hto3.SimpleSpecsDetector/Detectors/Linux/VideoDetector.cs
@@ -14,12 +14,84 @@ namespace Hto3.SimpleSpecsDetector.Detectors.Linux
     {        
         public String GetDisplayAdapterName()
         {
-            return null;
+            if (!File.Exists("/usr/bin/lspci"))
+                return null;
+
+            var stdout = new StringBuilder();
+            var processStartInfo = new ProcessStartInfo("lspci");
+            processStartInfo.RedirectStandardOutput = true;
+
+            using (var statProcess = Process.Start(processStartInfo))
+            {
+                var statProcessOutputDataReceived = new DataReceivedEventHandler((sender, e) => stdout.AppendLine(e.Data));
+                statProcess.OutputDataReceived += statProcessOutputDataReceived;
+                statProcess.BeginOutputReadLine();
+                statProcess.WaitForExit();
+            }
+
+            var match = Regex.Match(stdout.ToString(), @"VGA compatible controller:\s(?<value>.*)");
+
+            if (!match.Success)
+                return null;
+
+            var vgaCardName = match.Value;
+
+            return vgaCardName;
         }
 
         public Int64? GetVideoMemory()
         {
-            return 0;
+            if (!File.Exists("/usr/bin/lspci"))
+                return null;
+
+            var stdout = new StringBuilder();
+            var processStartInfo = new ProcessStartInfo("lspci");
+            processStartInfo.RedirectStandardOutput = true;
+
+            using (var statProcess = Process.Start(processStartInfo))
+            {
+                var statProcessOutputDataReceived = new DataReceivedEventHandler((sender, e) => stdout.AppendLine(e.Data));
+                statProcess.OutputDataReceived += statProcessOutputDataReceived;
+                statProcess.BeginOutputReadLine();
+                statProcess.WaitForExit();
+            }
+
+            var match = Regex.Match(stdout.ToString(), @"(?<value>\d{2}:\d{2}\.\d)\sVGA compatible controller:");
+
+            if (!match.Success)
+                return null;
+
+            stdout.Clear();
+
+            processStartInfo = new ProcessStartInfo("lspci", $"-v -s {match.Groups["value"].Value}");
+
+            using (var statProcess = Process.Start(processStartInfo))
+            {
+                var statProcessOutputDataReceived = new DataReceivedEventHandler((sender, e) => stdout.AppendLine(e.Data));
+                statProcess.OutputDataReceived += statProcessOutputDataReceived;
+                statProcess.BeginOutputReadLine();
+                statProcess.WaitForExit();
+            }
+
+            match = Regex.Match(stdout.ToString(), @"Memory\sat\s[^\[]+\[size=(?<size>\d+)(?<unit>\w)\]");
+
+            if (!match.Success)
+                return null;
+
+            var unit = match.Groups["unit"].Value;
+            var size = Int32.Parse(match.Groups["size"].Value);
+
+            switch (unit)
+            {
+                case "K":
+                    return size * 1000;
+                case "M":
+                    return size * 1000000;
+                case "G":
+                    return size * 1000000000;                
+                default:
+                    return null;
+            }
         }
 
         public USize? GetCurrentVideoResolution()
@@ -40,6 +112,9 @@ namespace Hto3.SimpleSpecsDetector.Detectors.Linux
             }
 
             var match = Regex.Match(stdout.ToString(), @"connected\s\w+\s(?<width>\d+)x(?<height>\d+)");
+
+            if (!match.Success)
+                return null;
 
             var width = UInt32.Parse(match.Groups["width"].Value);
             var height = UInt32.Parse(match.Groups["height"].Value);
