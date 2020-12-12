dc91716936e419eb8a1e5a7fa5b61aef63e0ee60
HTO3 <helio_net@hotmail.com>
2020-12-12 18:02:28 -0300

    Get Linux printers.

--- Hto3.SimpleSpecsDetector/Detectors/Linux/PrinterDetector.cs
+++ Hto3.SimpleSpecsDetector/Detectors/Linux/PrinterDetector.cs
@@ -2,6 +2,8 @@
 using Hto3.SimpleSpecsDetector.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -11,7 +13,36 @@ namespace Hto3.SimpleSpecsDetector.Detectors.Linux
     {
         public IEnumerable<PrinterDevice> GetPrinters()
         {
-            return Enumerable.Empty<PrinterDevice>();
+            if (!File.Exists("/usr/bin/lpstat"))
+                yield break;
+
+            var stdout = new StringBuilder();
+            var processStartInfo = new ProcessStartInfo("lpstat", "-a -d");
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.RedirectStandardError = true;
+
+            using (var statProcess = Process.Start(processStartInfo))
+            {
+                var statProcessOutputDataReceived = new DataReceivedEventHandler((sender, e) => stdout.AppendLine(e.Data));
+                statProcess.OutputDataReceived += statProcessOutputDataReceived;
+                statProcess.BeginOutputReadLine();
+                statProcess.WaitForExit();
+            }
+
+            var lpstatOutput = stdout.ToString().Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            var lastLpstatOutput = lpstatOutput[lpstatOutput.Length - 1];
+            var defaultPrinterName = lastLpstatOutput.Substring(lastLpstatOutput.IndexOf(':') + 2);
+
+            for (var i = 0; i < lpstatOutput.Length - 1; i++)
+            {
+                var printerName = lpstatOutput[i].Substring(0, lpstatOutput[i].IndexOf(' '));
+
+                yield return new PrinterDevice()
+                {
+                    DefaultPrinter = defaultPrinterName == printerName,
+                    Name = printerName
+                };
+            }
         }
     }
 }
